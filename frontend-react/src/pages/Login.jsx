05f5fa799b5c83fab879b3db5136974c24594af0
nguyenduyhien123 <93326941+nguyenduyhien123@users.noreply.github.com>
2023-07-26 19:06:03 +0700

    thiết kế giao diện đăng nhập, đăng ký

--- frontend-react/src/pages/Login.jsx
+++ frontend-react/src/pages/Login.jsx
@@ -1,4 +1,31 @@
+import { Alert, Button, Form, Row, Col, Stack } from "react-bootstrap";
 function Login() {
-  return <>Login</>;
+  return (
+    <>
+      <Form>
+        <Row
+          style={{
+            height: "100vh",
+            justifyContent: "center",
+            paddingTop: "10%",
+          }}
+        >
+          <Col xs={6}>
+            <Stack gap={3}>
+              <h2>Register</h2>
+              <Form.Control type="email" placeholder="Email" />
+              <Form.Control type="password" placeholder="Password" />
+              <Button variant="primary" type="submit">
+                Register
+              </Button>
+              <Alert variant="danger">
+                <p>An error occured</p>
+              </Alert>
+            </Stack>
+          </Col>
+        </Row>
+      </Form>
+    </>
+  );
 }
 export default Login;
